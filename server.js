8ca4593ec2c71215fcd8e6db7bbfb8b16d208849
Telahoun Molla <telahounmolla@gmail.com>
2019-06-21 09:58:38 -0700

    Fix intial commit issues

--- server.js
+++ server.js
@@ -13,11 +13,18 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 
+app.get("/api/example", (req, res) => {
+  console.log("hit");
+  res.json({example:"value"});
+});
+
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
